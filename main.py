a56aa1820bc785a217bd6cdbb1bf3c7d98891fca
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-10-30 21:51:41 -0300

    feat: estrutura básica

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import time
+
+st.image("assets/images/banner.png")
+st.title("Mercadinho do Paul")
+
+st.markdown("O Mercadinho do Paul é um estabelecimento comercial de bairro que oferece uma variedade de produtos para atender às necessidades do dia a dia. Com um ambiente acolhedor e atendimento personalizado, o mercadinho se destaca pela praticidade e pela proximidade com seus clientes.")
+st.logo("assets/images/logo/logo.png", size='large')   
+
