766196fce96c4c76607c0456c12c32db36657764
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-11-15 09:46:35 -0300

    fix: adicionando novo banco para inicializar

--- services/database.py
+++ services/database.py
@@ -8,7 +8,9 @@ def initDB():
     from services.categoria_service import initCategoriaTB
     from services.cliente_service import initClienteTB
     from services.produto_service import initProdutoTB
+    from services.funcionario_service import initFuncionarioTB
     conexao = conectaBD()
     initCategoriaTB(conexao=conexao)
     initClienteTB(conexao=conexao)
     initProdutoTB(conexao=conexao)
+    initFuncionarioTB(conexao=conexao)
